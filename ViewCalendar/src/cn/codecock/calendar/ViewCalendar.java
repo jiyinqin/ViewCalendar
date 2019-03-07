e5278af1c64cdcfb96bfda42ae61ee5ebfce2a78
jiyinqin <247846714@qq.com>
2019-03-07 18:40:30 +0800

    Version 1.0

---
+++ ViewCalendar/src/cn/codecock/calendar/ViewCalendar.java
@@ -0,0 +1,50 @@
+package cn.codecock.calendar;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ * 可视化日历程序
+ * @author l
+ *
+ */
+public class ViewCalendar {
+
+	public static void main(String[] args) throws ParseException {
+		System.out.println("请输入日期(格式：yyyy-MM-dd):2");
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		Date date =  df.parse(str);
+		Calendar c = new GregorianCalendar();
+		c.setTime(date);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		int days = c.getActualMaximum(Calendar.DATE);
+		System.out.println("日\t一\t二\t三\t四\t五\t六");	
+		c.set(Calendar.DAY_OF_MONTH, 1);		
+		for(int i=0;i<c.get(Calendar.DAY_OF_WEEK)-1;i++){
+			System.out.print("\t");
+		}
+		for(int i=1;i<=days;i++) {
+			if(day==c.get(Calendar.DAY_OF_MONTH)) {
+				System.out.print(c.get(Calendar.DAY_OF_MONTH)+"*\t");
+			}else {
+			System.out.print(c.get(Calendar.DAY_OF_MONTH)+"\t");
+			}
+			if(c.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY) {
+				System.out.println();     //每到周六换行
+			}
+			
+			c.add(Calendar.DAY_OF_MONTH, 1);
+			
+		}
+		
+		
+		
+	}
+}
